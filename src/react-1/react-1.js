13aae44171f2d0f149a6db58939090cd29157948
gitbanis <vvbanis@gmail.com>
2022-06-27 09:18:20 +0300

    react-1

---
+++ src/react-1/react-1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const React1 = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+React1.propTypes = {
+    
+};
+
+export default React1;
